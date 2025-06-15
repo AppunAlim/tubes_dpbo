e181805ace14a35fa15f8b553884ac209837c9a3
RifkyAL77 <rifkyalbuchorilearning@gmail.com>
2025-06-15 15:55:34 +0700

    update keranjang

--- src/Produk/PCGaming.java
+++ src/Produk/PCGaming.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Produk;
 
-/**
- *
- * @author AFFAN SALIM
- */
 public class PCGaming extends Produk{
     private String processor;
     private String gpu;
